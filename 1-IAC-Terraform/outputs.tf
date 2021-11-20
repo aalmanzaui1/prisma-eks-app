994ced1320a5f9b29edc2cda2455f11a5959d38e
Alvaro Almanza <alvaroalmanza@MacBook-Pro-de-Alvaro.local>
2021-11-20 13:14:49 +0100

    fit more generic cluster

--- 1-IAC-Terraform/outputs.tf
+++ 1-IAC-Terraform/outputs.tf
@@ -2,9 +2,9 @@ output "eks-creation-status" {
   value = aws_eks_cluster.eks-cluster.status
 }
 
-output "ecr-repository" {
-  value = aws_ecr_repository.ecr-repository.repository_url
-}
+# output "ecr-repository" {
+#   value = aws_ecr_repository.ecr-repository.repository_url
+# }
 
 output "external-dns-iam-role" {
   value = aws_iam_role.external-dns.arn
